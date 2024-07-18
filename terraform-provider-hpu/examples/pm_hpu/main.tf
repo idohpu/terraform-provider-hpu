0fbaa81d168984f71731dad8674f7f1b44a1ac83
Admin Pete Chee Zacharias <adminpz@ido-petetest-da1.hpu.edu>
2024-07-18 08:49:32 -1000

    errro

--- terraform-provider-hpu/examples/pm_hpu/main.tf
+++ terraform-provider-hpu/examples/pm_hpu/main.tf
@@ -10,8 +10,8 @@ terraform {
 provider "hpu" {
     pm_tls_insecure = true
     pm_api_url = "https://ido-proxmox-pa1.hpu.edu:8006/api2/json"
-    pm_api_token_id = "apiuser@pam!terraform_token_1"
-    pm_api_token_secret = "a5cce985-97c1-41ad-a3a3-959a77452de8"
+    pm_api_token_id = ""
+    pm_api_token_secret = ""
 }
 
 resource "hpu_proxmox_vm" "vm1" {
